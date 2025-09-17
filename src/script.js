cce63c793ffb837683bd7f2956477665dbb79f31
Vdujour <valentin.dujour@etu.univ-poitiers.fr>
2025-09-17 17:43:45 +0200

    Update characters list with data

--- src/script.js
+++ src/script.js
@@ -1,9 +1,18 @@
 function getCharacters() {
     fetch('http://localhost:3000/data/characters.json')
         .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error fetching data:', error));
+        .then(data => {
+            const characterList = document.getElementById('characters');
+            const ul = document.createElement('ul');
+            data.forEach(character => {
+                const li = document.createElement('li');
+                li.textContent = character.name;
+                characterList.appendChild(li);
+            });
+        });
 }
 
 // afficher dans la consoles
 getCharacters();
+
+
